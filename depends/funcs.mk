10e9fd0b500c6005b96dfb6f0b9673a775372e60
Jack Grigg <jack@z.cash>
2018-03-21 10:31:04 +0000

    depends: Move .cargo-checksum.json generation into standalone shell script

--- depends/funcs.mk
+++ depends/funcs.mk
@@ -32,7 +32,7 @@ endef
 
 define generate_crate_checksum
 rm .gitignore && \
-echo "{\"files\":{`find . -type f | grep -v "$($(1)_file_name)" | grep -v .stamp_ | sed 's|^./||' | sort | xargs $(build_SHA256SUM) | awk -v OFS='":"' '{print $$$$2, $$$$1}' | sed 's|^|"|' | sed 's|$|"|' | tr '\n' ',' | sed 's|,$$$$||'`},\"package\":\"$($(1)_sha256_hash)\"}" > .cargo-checksum.json
+$(BASEDIR)/cargo-checksum.sh "$($(1)_file_name)" "$(build_SHA256SUM)" "$($(1)_sha256_hash)"
 endef
 
 define vendor_crate_source
